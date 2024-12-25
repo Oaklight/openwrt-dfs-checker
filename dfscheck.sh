32996ed2bd3c35f28bad8d0327245594be6f41b8
Peng Ding <pding.dp@foxmail.com>
2024-12-25 13:31:14 -0600

    Refactor dfscheck.sh with function and validation

--- dfscheck.sh
+++ dfscheck.sh
@@ -1,10 +1,14 @@
-#! /bin/ash
+#!/bin/ash
 
-device="wlan$1-1"
-radio="wireless.radio$1.channel"
-channel="$2"
-fallbackChannel="$3"
+# Function to switch channel
+switch_channel() {
+    local channel=$1
+    uci set "$radio=$channel"
+    uci commit
+    wifi reload
+}
 
+# Validate arguments
 if [ $# -lt 3 ]; then
     echo "Usage: dfs-checker [device] [channel] [fallback_channel]"
     echo "  device:           Numeric (e.g. 0 translates to wlan0-1)"
@@ -13,26 +17,28 @@ if [ $# -lt 3 ]; then
     exit 1
 fi
 
+device="wlan$1-1"
+radio="wireless.radio$1.channel"
+channel="$2"
+fallbackChannel="$3"
+
 logger -s -t "DFS-checker" -p "user.warn" "DFS-checker has started. Device: $device, channel: $channel, fallback channel: $fallbackChannel"
-uci set wireless.radio0.channel="$channel"
-uci commit
-wifi reload
+
+# Set initial channel
+switch_channel "$channel"
 sleep 120 # Wait for normal wifi startup on boot
+
+# Main loop
 while true; do
-    iwinfo "$device" info 2>&1 | grep -q 'No such wireless device'
-    if [ $? == 0 ]; then
-        logger -s -t "DFS-checker" -p "user.warn" "$device is down, switch radio1 channel to $fallbackChannel for 30 minutes."
-        uci set wireless.radio0.channel="$fallbackChannel"
-        uci commit
-        wifi reload
-        sleep 1800 # Backoff time for radar detected, at least 30 mins?
-        logger -s -t "DFS-checker" -p "user.info" "switch radio1 channel back to $channel"
-        uci set wireless.radio0.channel="$channel"
-        uci commit
-        wifi reload
+    if iwinfo "$device" info 2>&1 | grep -q 'No such wireless device'; then
+        logger -s -t "DFS-checker" -p "user.warn" "$device is down, switching to fallback channel $fallbackChannel for 30 minutes."
+        switch_channel "$fallbackChannel"
+        sleep 1800 # Backoff time for radar detected, at least 30 mins
+        logger -s -t "DFS-checker" -p "user.info" "Switching back to main channel $channel"
+        switch_channel "$channel"
         sleep 75 # Give time for initial DFS scan, must >60s typ.
     else
-        echo "$device is ok"
+        logger -s -t "DFS-checker" -p "user.info" "$device is operating normally."
     fi
     sleep 15 # Check interval
 done
